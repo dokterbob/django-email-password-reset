e17649b6d70d4cedaff369cc77c7a66a252b27de
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2014-04-24 16:34:11 +0200

    README and setup.py

--- README.rst
+++ README.rst
@@ -2,3 +2,15 @@ django-password-reset
 =====================
 
 Minimalist app disabling all passwords and sending password reset emails.
+
+Usage
+-----
+1. Install app::
+
+       pip install django-password-reset
+
+2. Include `password_reset` app in `INSTALLED_APPS`.
+3. Setup password reset template in `password_reset/email.txt` and `password_reset/email_subject.txt`.
+4. Disable all passwords and send reset emails to all users with management command::
+
+       ./manage.py reset_passwords
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+try:
+    README = open('README.rst').read()
+except:
+    README = None
+
+setup(
+    name='django-password-reset',
+    version='0.1',
+    description='Minimalist app disabling all passwords and sending password reset emails.',
+    long_description=README,
+    license='BSD',
+    author='Mathijs de Bruin',
+    author_email='mathijs@mathijsfietst.nl',
+    url='https://github.com/dokterbob/django-password-reset/',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    )
+)
